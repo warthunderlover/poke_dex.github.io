18798ae335629c760103bdbb93dfa532124dbff8
warthunderlover <lazoemanuel29@gmail.com>
2025-04-04 17:52:02 -0600

    emanuel_3v

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: process.env.DEPLOY_BASE_URL || "/", // Esto asegura que funcione en GitHub Pages
-});
+  base: process.env.DEPLOY_BASE_URL,
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+})
