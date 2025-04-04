e3b788245509c5bdfd1fddb8dfa563e0ccce98e4
Orlando Betancourth <orlando@uledger.io>
2025-04-04 08:18:46 -0600

    fix: handle null values on my collection

--- src/pages/mycollection.jsx
+++ src/pages/mycollection.jsx
@@ -3,24 +3,26 @@ import { useNavigate } from 'react-router';
 import { useLocalStorage } from "../services/localStorage"
 import { PokemonList } from "../components/Pokemon/PokemonList";
 
-export const MyCollection = ()=>{
+export const MyCollection = () => {
     const [pokeCollection,] = useLocalStorage("mycollection");
     const [finalCollection, setFinalCollection] = useState([]);
     const navigate = useNavigate();
     useEffect(
-        ()=>{
-            setFinalCollection(Object.entries(pokeCollection).map(
-                ([, pokemon])=>{
-                    return pokemon
-                }
-            ));
+        () => {
+            if (pokeCollection) {
+                setFinalCollection(Object.entries(pokeCollection).map(
+                    ([, pokemon]) => {
+                        return pokemon
+                    }
+                ));
+            }
         }
         , [pokeCollection]
     );
     return (
         <PokemonList
             pokemonList={finalCollection}
-            clickHandler={(code)=>{
+            clickHandler={(code) => {
                 console.log("Code", code);
                 navigate(`/details/${code}`)
             }}
