02b21082cd8fda36dafbf3f5053ed8006e116a32
warthunderlover <lazoemanuel29@gmail.com>
2025-04-04 17:28:51 -0600

    pokedex

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.DEPLOY_BASE_URL,
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react()],
+  base: process.env.DEPLOY_BASE_URL || "/", // Esto asegura que funcione en GitHub Pages
+});
